0a6d4a70c639be378fa879f41dd85f0ce259b8be
Mirza_Md_Shan_Ali <88309339+Shan-Ali4@users.noreply.github.com>
2026-05-20 02:43:16 +0530

    sign-in_sign-up page

--- spott/components/ui/header.jsx
+++ spott/components/ui/header.jsx
@@ -2,18 +2,19 @@ import Link from 'next/link'
 import Image from 'next/image'
 import React from 'react'
 import { SignInButton, SignUpButton, UserButton, Show } from '@clerk/nextjs'
-import { Button } from '@base-ui/react'
+import { Button } from "@/components/ui/button";
+
 
 const Header = () => {
     return (
         <>
-            <nav className='fixed top-0 left-0 right-0 bg-background/80 backdrop-blur-xl z-20 border-b'>
-                <div className='max-w-7xl mx-auto px-6 flex items-center justify-between'>
+            <nav className="fixed top-0 left-0 right-0 bg-background/80 backdrop-blur-xl z-20 border-b">
+                <div className='max-w-7xl mx-auto px-6 py-4 flex items-center justify-between'>
                     {/* Logo */}
                     <Link href={'/'} className='flex items-center'>
                         <Image
                             src="/spott.png"
-                            alt="SPOTT Logo"
+                            alt="Spott logo"
                             width={500}
                             height={500}
                             className="w-full h-11"
@@ -26,8 +27,8 @@ const Header = () => {
                     {/* Right Side Action */}
                     <div className='flex items-center'>
                         <Show when="signed-out">
-                            <SignInButton>
-                                <button size="sm">Sign In</button>
+                            <SignInButton mode="modal">
+                                <Button size="sm">Sign In</Button>
                             </SignInButton>
                         </Show>
                         <Show when="signed-in">
