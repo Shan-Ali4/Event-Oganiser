0ff07bad31495061fdbb3a0b91721b764add1a11
Mirza_Md_Shan_Ali <88309339+Shan-Ali4@users.noreply.github.com>
2026-05-23 02:06:01 +0530

    schema building

--- spott/components/ui/header.jsx
+++ spott/components/ui/header.jsx
@@ -2,18 +2,19 @@
 
 import Link from 'next/link'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 import { SignInButton, SignUpButton, UserButton, Show } from '@clerk/nextjs'
 import { Button } from "@/components/ui/button";
 import { Authenticated, Unauthenticated } from "convex/react";
 import { BarLoader } from 'react-spinners';
 import { useStoreUser } from '@/hooks/use-store-user';
+import { Building, Plus, Ticket } from 'lucide-react';
 
 
 const Header = () => {
 
     const { isLoading } = useStoreUser();
-
+    const [showUpgradeModal, setShowUpgradeModal] = useState(false);
     return (
         <>
             <nav className="fixed top-0 left-0 right-0 bg-background/80 backdrop-blur-xl z-20 border-b">
@@ -34,16 +35,40 @@ const Header = () => {
                     {/* Search & Location -  Desktop Only */}
                     {/* Right Side Action */}
                     <div className='flex items-center'>
+                        <Button variant={"ghost"} size="sm" onClick={() => setShowUpgradeModal(true)}>Pricing</Button>                        
+                        <Button variant="ghost" size="sm" asChild className={"mr-2"}>
+                            <Link href="/explore">Explore</Link>
+                        </Button>
+                        <Authenticated>
+                            <Button size="sm" asChild className="flex gap-2 mr-4">
+                                <Link href="/create-event">
+                                <Plus className="w-4 h-4" />
+                                <span className="hidden sm:inline">Create Event</span>
+                                </Link>
+                            </Button>
+                            <UserButton>
+                                <UserButton.MenuItems> 
+                                    <UserButton.Link
+                                    label="My Tickets"
+                                    labelIcon={<Ticket size={16} />}
+                                    href="/my-tickets"
+                                    />
+                                    <UserButton.Link
+                                    label="My Events"
+                                    labelIcon={<Building size={16} />}
+                                    href="/my-events"
+                                    />
+                                    <UserButton.Action label="manageAccount" />
+                                </UserButton.MenuItems>
+                            </UserButton>
+                        </Authenticated>
                         <Unauthenticated>
                             <SignInButton mode="modal">
                                 <Button size="sm">Sign In</Button>
                             </SignInButton>
                         </Unauthenticated>
-                        <Authenticated>
-                            <UserButton />
-                        </Authenticated>
-                    </div>
-                </div>
+                            </div>
+                        </div>
 
                 {/* Mobile Search & Location - Below Header */}
 
@@ -52,8 +77,6 @@ const Header = () => {
                     <BarLoader width="100%" color= "purple"/>
                 </div>}
             </nav>
-
-            {/* Models */}
         </>
     )
 }
